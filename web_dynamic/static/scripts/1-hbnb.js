d9054d1f3f9a37222411ace0d28654a07212f6d3
rodrigoandresd <rodrigoaduqueg@gmail.com>
2023-01-04 17:10:19 -0500

    finished task 1

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,1 +1,21 @@
-
+$(document).ready(() => {
+  let xCheck = [];
+  let amenitiesName = [];
+  $("input[type='checkbox']").change(function () {
+    if (this.checked) {
+      xCheck.push($(this).attr("data-id"));
+      amenitiesName.push($(this).attr("data-name"));
+      $(".amenities h4").text(amenitiesName);
+    }
+    else {
+      let index = xCheck.indexOf($(this).attr("data-id"));
+      xCheck.splice(index, 1);
+      amenitiesName.splice(index, 1);
+      if (amenitiesName.length > 0) {
+        $(".amenities h4").text(amenitiesName);
+      } else {
+        $(".amenities h4").html('&nbsp;');
+      }
+    }
+  });
+});
